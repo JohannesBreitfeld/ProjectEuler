8b1de5d4f287c7650b68c87cc838e3fc44b5a7b2
JohannesBreitfeld <jbvbreitfeld@gmail.com>
2024-09-20 12:54:19 +0200

    Add Problem009, 010 and 011.

---
+++ Problem009/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 0, b = 0, c = 0;
+bool isFound = false;
+
+for (int i = 1; i < 335; i++)
+{
+	for (int j = 0; j < 1000-i; j++)
+	{
+		int k = 1000 - i - j;
+		if (Math.Pow(i, 2) + Math.Pow(j, 2) == Math.Pow(k, 2))
+		{
+			a = i;
+			b = j;
+			c = k;
+			isFound = true;
+			break;
+
+		}
+	}
+	if (isFound) 
+	{ 
+		break; 
+	}
+}
+Console.WriteLine($"a = {a} b = {b} c = {c}"); // a= 200, b=375, c=425
+Console.WriteLine($"a*b*c = {a * b * c}"); //31875000
---
+++ Problem010/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+
+long sumOfPrimes = 5;
+bool isPrime;
+int currentNumber = 3;
+
+while (currentNumber < 2000000)
+{
+    currentNumber += 2;
+    isPrime = IsPrime(currentNumber);
+    if (isPrime)
+    {
+        sumOfPrimes += currentNumber;
+    }
+}
+
+Console.Write(sumOfPrimes); // 142.913.828.922
+
+
+static bool IsPrime(int value)
+{
+    for (int divisor = 3; divisor * divisor <= value; divisor += 2)
+    {
+        if (value % divisor == 0) return false;
+    }
+
+    return true;
+}
