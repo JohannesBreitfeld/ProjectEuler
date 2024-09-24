d64c24e1ed8507566e36163b966c4831dd30c1e2
JohannesBreitfeld <jbvbreitfeld@gmail.com>
2024-09-24 19:09:19 +0200

    Added Problem012

---
+++ Problem012/Program.cs
@@ -0,0 +1,34 @@
+﻿long term = 0;
+int divisorCounter = 0;
+int counter = 0;
+
+while (true)
+{
+    counter++;
+    term += counter;
+
+    divisorCounter = NumberOfDivisors(term);
+
+    if (divisorCounter > 500)
+    {
+        break;
+    }
+
+}
+
+Console.WriteLine(term); //76576500
+Console.WriteLine(divisorCounter); //576 divisors
+
+
+static int NumberOfDivisors(long term)
+{
+    int divisors = 0;
+    for (int i = 1; i <= Math.Sqrt(term); i++)
+    {
+        if (term % i == 0)
+        {
+            divisors += 2;
+        }
+    }
+    return divisors;
+}
