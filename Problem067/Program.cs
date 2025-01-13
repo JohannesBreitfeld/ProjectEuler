a41c622f538f6d59257160da65b8bb59b76e85c7
JohannesBreitfeld <jbvbreitfeld@gmail.com>
2025-01-13 23:33:47 +0100

    Add project for problem 067

---
+++ Problem067/Program.cs
@@ -0,0 +1,22 @@
+﻿var path = Path.Combine("..", "..", "..", "0067_triangle.txt");
+
+var input = File.ReadAllLines(path);
+
+var triangle = new List<List<int>>();
+
+foreach (var line in input)
+{
+    var row = line.Split(" ").Select(n => int.Parse(n)).ToList();
+    triangle.Add(row);
+}
+
+for (int row = triangle.Count - 2; row >= 0; row--)
+{
+    for (int col = 0; col <= row; col++)
+    {
+        triangle[row][col] += Math.Max(triangle[row + 1][col], triangle[row + 1][col + 1]);
+    }
+}
+
+var result = triangle[0][0];
+Console.WriteLine(result);
